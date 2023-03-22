8cead2fc4bbbc58d49ee1346ed8f4c5769600520
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-03-22 17:00:49 +0530

    initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+// Pascal annotation
+function Message() {
+  // jsx js + HTML
+  const name = "polo";
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>lorem</h1>
+    </div>
+  );
+}
+
+export default Message;
